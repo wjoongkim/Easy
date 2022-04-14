b2d1e350947d50901288a4b29f56e22ce7c36464
hm-gwon <howling6@naver.com>
2022-04-14 14:33:54 +0900

    Git Push & PR Test by HM.GWON

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     static public void main(String[] args) throws IOException {
         System.out.println("Hello Easy Member");
         System.out.println("Go! Go! Team Project");
+        System.out.println("Hello Code Review World!");
     }
 }
