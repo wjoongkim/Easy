9ac6bf894df2b72f5f7d507503abad52e6200aca
hm-gwon <howling6@naver.com>
2022-04-18 15:20:09 +0900

    최신 클래스 현황에 맞게 Modify관련 코드 수정된 버전 올려드립니다.
    받으셔서 오류 삭제부탁드립니다.

--- src/main/java/Modify.java
+++ src/main/java/Modify.java
@@ -10,64 +10,31 @@ public class Modify extends CRUDManager {
 
         if (token.length < 7) return null; //입력이 올바르지 않는 경우 예외처리합니다.
 
-        String printOption = token[1];
-        ArrayList<String> searchResult = Search(employeeMap, token[2], token[4], token[5]);
+        //List<String> searchResult = Search(employeeMap, token[2], token[4], token[5]);
+        ArrayList<String> searchResult = getEmpNoList(employeeMap, args);
 
         if (searchResult == null) return token[0] + ",NONE"; //검색 결과가 없는 경우 예외처리합니다.
 
-        if (printOption.compareTo("-p") == 0) {
-            Set<String> searchfilter = searchResult.stream().collect(Collectors.toCollection(HashSet::new));
-            result = employeeMap.entrySet().stream().filter(c -> searchfilter.contains(c.getKey())).sorted(new Comparator<Map.Entry<String, Employee>>() {
-                @Override
-                public int compare(Map.Entry<String, Employee> o1, Map.Entry<String, Employee> o2) {
-                    return o1.getValue().getJoinYear() - o2.getValue().getJoinYear();
-                }
-            }).limit(5).map(c -> c.getValue().getEmpInfo("MOD")).collect(Collectors.joining("\n"));
+        if (token[1].compareTo("-p") == 0) {
+            result = getPOption(employeeMap, searchResult).stream().map(e -> e.getEmpInfo(token[0])).collect(Collectors.joining("\n"));
         } else {
             result = token[0] + "," + searchResult.size();
         }
-
         modify(employeeMap, searchResult, token[6], token[7]);
+
         return result;
     }
-
-    ArrayList<String> Search(Map<String, Employee> employeeMap, String option, String column, String param) {
-        try {
-            switch (column) {
-                case "employeeNum":
-                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getEmployeeNum().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-                case "name":
-                    return nameSearchStream(employeeMap, option, param);
-                case "phoneNum":
-                    return phoneNumSearchStream(employeeMap, option, param);
-                case "cl":
-                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getCl().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-                case "birthday":
-                    return birthdaySearchStream(employeeMap, option, param);
-                case "certi":
-                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getCerti().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-                default:
-                    return null;
-            }
-        } catch (Exception e) {
-            System.out.println("MOD IN STREAM SEARCH ERROR");
-        }
-        return null;
-    }
-
 
     void modify(Map<String, Employee> employeeMap, ArrayList<String> data, String column, String param) {
         switch (column) {
             case "name":
-                String[] split_name = param.split(" ");
                 for (String Empno : data) {
-                    employeeMap.get(Empno).setName(new Name(param, split_name[0], split_name[1]));
+                    employeeMap.get(Empno).setName(new Name(param));
                 }
                 break;
             case "phoneNum":
-                String[] split_Num = param.split("-");
                 for (String Empno : data) {
-                    employeeMap.get(Empno).setPhoneNum(new Phone(param, split_Num[1], split_Num[2]));
+                    employeeMap.get(Empno).setPhoneNum(new Phone(param));
                 }
                 break;
             case "cl":
@@ -89,6 +56,30 @@ public class Modify extends CRUDManager {
                 //System.out.println("ERROR");
                 break;
         }
+    }
+
+    List<String> Search(Map<String, Employee> employeeMap, String option, String column, String param) {
+        try {
+            switch (column) {
+                case "employeeNum":
+                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getEmployeeNum().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
+                case "name":
+                    return nameSearchStream(employeeMap, option, param);
+                case "phoneNum":
+                    return phoneNumSearchStream(employeeMap, option, param);
+                case "cl":
+                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getCl().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
+                case "birthday":
+                    return birthdaySearchStream(employeeMap, option, param);
+                case "certi":
+                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getCerti().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toList());
+                default:
+                    return null;
+            }
+        } catch (Exception e) {
+            System.out.println("MOD IN STREAM SEARCH ERROR");
+        }
+        return null;
     }
 
     ArrayList<String> nameSearchStream(Map<String, Employee> employeeMap, String option, String param) {
