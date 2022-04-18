980e25996bf93c72f6f03f43ec959661a8cdebb0
wjoongkim <wjoongkim@gmail.com>
2022-04-18 12:58:19 +0900

    추가로 코멘트해주신, Phone와 Name 클래스를 수정하였습니다. 나중에 사용할 input.txt 파일도 같이 추가 했습니다.

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -7,16 +7,6 @@ public class Add extends CRUDManager {
     String calc(Map<String, Employee> employeeMap, String args) {
         String[] tokens = args.split(",");
 
-        String fullName = tokens[5];
-        String[] nameTokens = fullName.split(" ");
-        String firstName = nameTokens[0];
-        String lastName = nameTokens[1];
-
-        String fullPhoneNumber = tokens[7];
-        String[] phoneNumberTokens = fullPhoneNumber.split("-");
-        String middlePhoneNumber = phoneNumberTokens[1];
-        String lastPhoneNumber = phoneNumberTokens[2];
-
         String joinYear = "19" + tokens[4].substring(0, 2);
         int intJoinYear = Integer.parseInt(joinYear);
         final int FIRST_YEAR_OF_EMPLOYMENT = 1969;
@@ -26,8 +16,7 @@ public class Add extends CRUDManager {
         }
 
         employeeMap.put(tokens[4], new Employee(
-                tokens[4], new Name(fullName, firstName, lastName), tokens[6]
-                , new Phone(fullPhoneNumber, middlePhoneNumber, lastPhoneNumber)
+                tokens[4], new Name(tokens[5]), tokens[6], new Phone(tokens[7])
                 , new Birthday(tokens[8]), tokens[9], intJoinYear * 1000000 + Integer.parseInt(tokens[4].substring(2))));
 
         return null;
@@ -67,8 +56,8 @@ public class Add extends CRUDManager {
         }
 
         Employee employee = new Employee(
-                tokens[4], new Name(fullName, firstName, lastName), tokens[6]
-                , new Phone(fullPhoneNumber, middlePhoneNumber, lastPhoneNumber)
+                tokens[4], new Name(fullName), tokens[6]
+                , new Phone(fullPhoneNumber)
                 , new Birthday(tokens[8]), tokens[9], intJoinYear * 1000000 + Integer.parseInt(tokens[4].substring(2)));
 
         employees.add(employee);
