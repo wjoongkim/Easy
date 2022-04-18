5aae1e0430f6af820be71e54d9913f4573ae8541
wjoongkim <wjoongkim@gmail.com>
2022-04-18 14:03:48 +0900

    추가로 코멘트해주신, 파싱 변수 분리하는 부분 반영했습니다.

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -7,7 +7,14 @@ public class Add extends CRUDManager {
     String calc(Map<String, Employee> employeeMap, String args) {
         String[] tokens = args.split(",");
 
-        String joinYear = "19" + tokens[4].substring(0, 2);
+        final int EMPNO = 4;
+        final int NAME = 5;
+        final int CL = 6;
+        final int PHONE = 7;
+        final int BIRTHDAY = 8;
+        final int CERTI = 9;
+
+        String joinYear = "19" + tokens[EMPNO].substring(0, 2);
         int intJoinYear = Integer.parseInt(joinYear);
         final int FIRST_YEAR_OF_EMPLOYMENT = 1969;
 
@@ -15,15 +22,9 @@ public class Add extends CRUDManager {
             intJoinYear += 100;
         }
 
-        final String EMPNO = tokens[4];
-        final String NAME = tokens[5];
-        final String CL = tokens[6];
-        final String PHONE = tokens[7];
-        final String BIRTHDAY = tokens[8];
-        final String CERTI = tokens[9];
-        final int JOINYEAR = intJoinYear * 1000000 + Integer.parseInt(tokens[4].substring(2));
+        final int JOINYEAR = intJoinYear * 1000000 + Integer.parseInt(tokens[EMPNO].substring(2));
 
-        employeeMap.put(EMPNO, new Employee(EMPNO, new Name(NAME), CL, new Phone(PHONE), new Birthday(BIRTHDAY), CERTI, JOINYEAR));
+        employeeMap.put(tokens[EMPNO], new Employee(tokens[EMPNO], new Name(tokens[NAME]), tokens[CL], new Phone(tokens[PHONE]), new Birthday(tokens[BIRTHDAY]), tokens[CERTI], JOINYEAR));
 
         return null;
     }
