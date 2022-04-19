064320616fc5148912a15893c8c4a516f1e65612
hm-gwon <howling6@naver.com>
2022-04-19 11:21:02 +0900

    Modify 리팩토링 및 Test에 Mock테스트 수정 및 최신화제출합니다.

--- src/main/java/Modify.java
+++ src/main/java/Modify.java
@@ -18,7 +18,7 @@ public class Modify extends CRUDManager {
 
         if (searchResult.size() == 0) return token[0] + ",NONE";//검색 결과가 없는 경우 예외처리합니다.
 
-        if (token[1].compareTo("-p") == 0) {
+        if (isPrintCondition(token[1])) {
             result = getPOption(searchResult).stream().map(e -> e.getEmpInfo(token[0])).collect(Collectors.joining("\n"));
         } else {
             result = token[0] + "," + searchResult.size();
@@ -28,95 +28,30 @@ public class Modify extends CRUDManager {
         return result;
     }
 
-    void modify(Map<String, Employee> employeeMap, ArrayList<String> data, String column, String param) {
+    private boolean isPrintCondition(String op) {
+        return op.compareTo("-p") == 0;
+    }
+
+    private void modify(Map<String, Employee> employeeMap, ArrayList<String> data, String column, String param) {
         switch (column) {
             case "name":
-                for (String Empno : data) {
-                    employeeMap.get(Empno).setName(new Name(param));
-                }
+                data.stream().forEach(e -> employeeMap.get(e).setName(new Name(param)));
                 break;
             case "phoneNum":
-                for (String Empno : data) {
-                    employeeMap.get(Empno).setPhoneNum(new Phone(param));
-                }
+                data.stream().forEach(e -> employeeMap.get(e).setPhoneNum(new Phone(param)));
                 break;
             case "cl":
-                for (String Empno : data) {
-                    employeeMap.get(Empno).setCl(param);
-                }
+                data.stream().forEach(e -> employeeMap.get(e).setCl(param));
                 break;
             case "birthday":
-                for (String Empno : data) {
-                    employeeMap.get(Empno).setBirthday(new Birthday(param));
-                }
+                data.stream().forEach(e -> employeeMap.get(e).setBirthday(new Birthday(param)));
                 break;
             case "certi":
-                for (String Empno : data) {
-                    employeeMap.get(Empno).setCerti(param);
-                }
+                data.stream().forEach(e -> employeeMap.get(e).setCerti(param));
                 break;
             default:
                 //System.out.println("ERROR");
                 break;
         }
-    }
-
-    List<String> Search(Map<String, Employee> employeeMap, String option, String column, String param) {
-        try {
-            switch (column) {
-                case "employeeNum":
-                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getEmployeeNum().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-                case "name":
-                    return nameSearchStream(employeeMap, option, param);
-                case "phoneNum":
-                    return phoneNumSearchStream(employeeMap, option, param);
-                case "cl":
-                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getCl().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-                case "birthday":
-                    return birthdaySearchStream(employeeMap, option, param);
-                case "certi":
-                    return employeeMap.entrySet().stream().filter(e -> e.getValue().getCerti().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toList());
-                default:
-                    return null;
-            }
-        } catch (Exception e) {
-            System.out.println("MOD IN STREAM SEARCH ERROR");
-        }
-        return null;
-    }
-
-    ArrayList<String> nameSearchStream(Map<String, Employee> employeeMap, String option, String param) {
-        if (option.compareTo("-f") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getName().getFirstName().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo("-l") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getName().getLastName().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo(" ") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getName().getFullName().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        }
-        return null;
-    }
-
-    ArrayList<String> phoneNumSearchStream(Map<String, Employee> employeeMap, String option, String param) {
-        if (option.compareTo("-m") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getPhoneNum().getMiddleNumber().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo("-l") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getPhoneNum().getLastNumber().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo(" ") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getPhoneNum().getFullNumber().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        }
-        return null;
-    }
-
-    ArrayList<String> birthdaySearchStream(Map<String, Employee> employeeMap, String option, String param) {
-        if (option.compareTo("-y") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getBirthday().getYear().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo("-m") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getBirthday().getMonth().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo("-d") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getBirthday().getDay().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        } else if (option.compareTo(" ") == 0) {
-            return employeeMap.entrySet().stream().filter(e -> e.getValue().getBirthday().getBirthday().compareTo(param) == 0).map(map -> map.getKey()).collect(Collectors.toCollection(ArrayList::new));
-        }
-        return null;
     }
 }
