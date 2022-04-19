6e56280c13bcaf4d81bec77ecec95fb741e65183
Jae Jung <jae47.jung@gmail.com>
2022-04-19 10:59:03 +0900

    getAdd / getDelete로 변경 TEST FILE

--- src/test/java/DelFunctionTest.java
+++ src/test/java/DelFunctionTest.java
@@ -4,12 +4,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,7 +28,7 @@ public class DelFunctionTest {
         Name n2 = new Name("FB NTAWR");
         Birthday b2 = new Birthday("19861203");
 
-        //String testString1 = "ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV";
+        String testString = "ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV";
         //String testString2 = "ADD, , , ,17112609,FB NTAWR,CL4,010-5645-6122,19861203,PRO";
 
         Map<String, Employee> employeeMap = new HashMap<>();
@@ -41,19 +38,16 @@ public class DelFunctionTest {
         String testString1 = "ADD1";
         String testString2 = "ADD2";
 
-        //when(employeeManager.add(testString1)).thenReturn(employeeMap.put(e1.getEmployeeNum(), e1));
+        CRUDManager deleteManager = new Delete(employeeMap);
+
+        //when(employeeManager.getAdd().calc(testString1)).thenReturn(employeeManager.getAdd().calc(testString));
         //when(employeeManager.add(testString2)).thenReturn(employeeMap.put(e2.getEmployeeNum(), e2));
 
         //employeeManager.add(testString1);
         //employeeManager.add(testString2);
         employeeMap.put(e1.getEmployeeNum(), e1);
         employeeMap.put(e1.getEmployeeNum(), e2);
-        CRUDManager deleteManager = new Delete(employeeMap);
-
-        for (Employee e : deleteManager.employeeMap.values()) {
-            System.out.println(e.getName().getFullName());
-        }
-
+        //CRUDManager deleteManager = new Delete(employeeMap);
 
         //juint
         assertThat(deleteManager.employeeMap.size()).isEqualTo(2);
@@ -74,18 +68,18 @@ public class DelFunctionTest {
         String testString3 = "ADD, , , ,15123100,VXIHXOTH JHIP,CL3,010-3112-2609,19771211,ADV";
         String testString4 = "ADD, , , ,15123101,VXIHXOTH JHXP,CL3,010-3112-2609,19771211,ADV";
         EmployeeManager employeeManager = new EmployeeManager();
-        employeeManager.add.calc(testString1);
-        employeeManager.add.calc(testString2);
-        employeeManager.add.calc(testString3);
-        employeeManager.add.calc(testString4);
+        employeeManager.getAdd().calc(testString1);
+        employeeManager.getAdd().calc(testString2);
+        employeeManager.getAdd().calc(testString3);
+        employeeManager.getAdd().calc(testString4);
 
         //juint
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(4);
-        for (Employee e : employeeManager.employeeMap.values()) {
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(4);
+        for (Employee e : employeeManager.getEmployeeMap().values()) {
             System.out.println(e.getName().getFullName());
         }
-        assertThat(employeeManager.delete.calc("DEL,-p,-f, ,name,VXIHXOTH")).isEqualTo("DEL,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV" + "\n" + "DEL,15123100,VXIHXOTH JHIP,CL3,010-3112-2609,19771211,ADV" + "\n" + "DEL,15123101,VXIHXOTH JHXP,CL3,010-3112-2609,19771211,ADV");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(1);
+        assertThat(employeeManager.getDelete().calc("DEL,-p,-f, ,name,VXIHXOTH")).isEqualTo("DEL,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV" + "\n" + "DEL,15123100,VXIHXOTH JHIP,CL3,010-3112-2609,19771211,ADV" + "\n" + "DEL,15123101,VXIHXOTH JHXP,CL3,010-3112-2609,19771211,ADV");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(1);
     }
 
     // full name 삭제 테스트
@@ -96,20 +90,20 @@ public class DelFunctionTest {
         String testString3 = "ADD, , , ,15123100,VXIHXOTH JHIP,CL3,010-3112-2609,19771211,ADV";
         String testString4 = "ADD, , , ,15123101,VXIHXOTH JHXP,CL3,010-3112-2609,19771211,ADV";
         EmployeeManager employeeManager = new EmployeeManager();
-        employeeManager.add.calc(testString1);
-        employeeManager.add.calc(testString2);
-        employeeManager.add.calc(testString3);
-        employeeManager.add.calc(testString4);
+        employeeManager.getAdd().calc(testString1);
+        employeeManager.getAdd().calc(testString2);
+        employeeManager.getAdd().calc(testString3);
+        employeeManager.getAdd().calc(testString4);
 
         //juint
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(4);
-        for (Employee e : employeeManager.employeeMap.values()) {
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(4);
+        for (Employee e : employeeManager.getEmployeeMap().values()) {
             System.out.println(e.getName().getFullName());
         }
-        assertThat(employeeManager.delete.calc("DEL,-p, , ,name,VXIHXOTH JHOP")).isEqualTo("DEL,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(3);
-        assertThat(employeeManager.delete.calc("DEL,-p,-l, ,name,JHIP")).isEqualTo("DEL,15123100,VXIHXOTH JHIP,CL3,010-3112-2609,19771211,ADV");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(2);
+        assertThat(employeeManager.getDelete().calc("DEL,-p, , ,name,VXIHXOTH JHOP")).isEqualTo("DEL,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(3);
+        assertThat(employeeManager.getDelete().calc("DEL,-p,-l, ,name,JHIP")).isEqualTo("DEL,15123100,VXIHXOTH JHIP,CL3,010-3112-2609,19771211,ADV");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(2);
     }
 
     // empno, phone 삭제 테스트
@@ -120,24 +114,24 @@ public class DelFunctionTest {
         String testString3 = "ADD, , , ,15123100,VXIHXOTH JHIP,CL3,010-3112-2611,19771211,ADV";
         String testString4 = "ADD, , , ,15123101,VXIHXOTH JHXP,CL3,010-3112-2611,19771211,ADV";
         EmployeeManager employeeManager = new EmployeeManager();
-        employeeManager.add.calc(testString1);
-        employeeManager.add.calc(testString2);
-        employeeManager.add.calc(testString3);
-        employeeManager.add.calc(testString4);
+        employeeManager.getAdd().calc(testString1);
+        employeeManager.getAdd().calc(testString2);
+        employeeManager.getAdd().calc(testString3);
+        employeeManager.getAdd().calc(testString4);
 
         //juint
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(4);
-        assertThat(employeeManager.delete.calc("DEL, , , ,employeeNum,17112609")).isEqualTo("DEL,1");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(3);
-        assertThat(employeeManager.delete.calc("DEL, , , ,employeeNum,17112609")).isEqualTo("DEL,NONE");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(3);
-        assertThat(employeeManager.delete.calc("DEL,-p,-m, ,phoneNum,2611")).isEqualTo("DEL,NONE");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(3);
+        assertEquals(employeeManager.getEmployeeMap().size(), 4);
+        assertThat(employeeManager.getDelete().calc("DEL, , , ,employeeNum,17112609")).isEqualTo("DEL,1");
+        assertEquals(employeeManager.getEmployeeMap().size(), 3);
+        assertThat(employeeManager.getDelete().calc("DEL, , , ,employeeNum,17112609")).isEqualTo("DEL,NONE");
+        assertEquals(employeeManager.getEmployeeMap().size(), 3);
+        assertThat(employeeManager.getDelete().calc("DEL,-p,-m, ,phoneNum,2611")).isEqualTo("DEL,NONE");
+        assertEquals(employeeManager.getEmployeeMap().size(), 3);
         //System.out.println(employeeManager.delete.calc("DEL,-p,-l, ,phoneNum,2611"));
-        assertThat(employeeManager.delete.calc("DEL,-p,-l, ,phoneNum,2611")).isEqualTo("DEL,15123100,VXIHXOTH JHIP,CL3,010-3112-2611,19771211,ADV\nDEL,15123101,VXIHXOTH JHXP,CL3,010-3112-2611,19771211,ADV");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(1);
-        assertThat(employeeManager.delete.calc("DEL, , , ,phoneNum,010-3112-2609")).isEqualTo("DEL,1");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(0);
+        assertThat(employeeManager.getDelete().calc("DEL,-p,-l, ,phoneNum,2611")).isEqualTo("DEL,15123100,VXIHXOTH JHIP,CL3,010-3112-2611,19771211,ADV\nDEL,15123101,VXIHXOTH JHXP,CL3,010-3112-2611,19771211,ADV");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(1);
+        assertThat(employeeManager.getDelete().calc("DEL, , , ,phoneNum,010-3112-2609")).isEqualTo("DEL,1");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(0);
     }
 
     // certi, cl, birth 삭제 테스트
@@ -150,26 +144,26 @@ public class DelFunctionTest {
         EmployeeManager employeeManager = new EmployeeManager();
         //Map<String, Employee> employeeMap = new HashMap<>();
         //CRUDManager addManager = new Add(employeeMap);
-        employeeManager.add.calc(testString1);
-        employeeManager.add.calc(testString2);
-        employeeManager.add.calc(testString3);
-        employeeManager.add.calc(testString4);
+        employeeManager.getAdd().calc(testString1);
+        employeeManager.getAdd().calc(testString2);
+        employeeManager.getAdd().calc(testString3);
+        employeeManager.getAdd().calc(testString4);
 
         //CRUDManager crudManager = new Delete(employeeMap);
         //juint
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(4);
-        assertThat(employeeManager.delete.calc("DEL,-p,-y, ,birthday,1979")).isEqualTo("DEL,15123101,VXIHXOTH JHXP,CL3,010-3112-2611,19791210,ADV");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(3);
-        assertThat(employeeManager.delete.calc("DEL, , , ,birthday,19781212")).isEqualTo("DEL,1");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(2);
-        assertThat(employeeManager.delete.calc("DEL,-p,-m, ,birthday,12")).isEqualTo("DEL,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(1);
-        assertThat(employeeManager.delete.calc("DEL, ,-d, ,birthday,10")).isEqualTo("DEL,NONE");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(1);
-        assertThat(employeeManager.delete.calc("DEL, , , ,certi,ADV")).isEqualTo("DEL,NONE");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(1);
-        assertThat(employeeManager.delete.calc("DEL,-p, , ,cl,CL4")).isEqualTo("DEL,17112609,FB NTAWR,CL4,010-5645-6122,19861103,PRO");
-        assertThat(employeeManager.employeeMap.size()).isEqualTo(0);
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(4);
+        assertThat(employeeManager.getDelete().calc("DEL,-p,-y, ,birthday,1979")).isEqualTo("DEL,15123101,VXIHXOTH JHXP,CL3,010-3112-2611,19791210,ADV");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(3);
+        assertThat(employeeManager.getDelete().calc("DEL, , , ,birthday,19781212")).isEqualTo("DEL,1");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(2);
+        assertThat(employeeManager.getDelete().calc("DEL,-p,-m, ,birthday,12")).isEqualTo("DEL,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(1);
+        assertThat(employeeManager.getDelete().calc("DEL, ,-d, ,birthday,10")).isEqualTo("DEL,NONE");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(1);
+        assertThat(employeeManager.getDelete().calc("DEL, , , ,certi,ADV")).isEqualTo("DEL,NONE");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(1);
+        assertThat(employeeManager.getDelete().calc("DEL,-p, , ,cl,CL4")).isEqualTo("DEL,17112609,FB NTAWR,CL4,010-5645-6122,19861103,PRO");
+        assertThat(employeeManager.getEmployeeMap().size()).isEqualTo(0);
     }
 
     /*
